e3cd9abb3f6fe2aa9750223601c09f5265a977f8
apjama <apjama@icloud.com>
2020-10-25 15:39:31 +0000

    delted duplicate organisation models file

--- db/models/organization.js
+++ db/models/organization.js
@@ -2,28 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const OrganizationSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    max: 100,
-  },
-  city: {
-    type: String,
-    required: true,
-    max: 100,
-  },
-  phoneNumber: {
-    type: String,
-    max: 100,
-    // validate: {
-    //     //   validator: function (v) {
-    //     //     return /\d{3}-\d{3}-\d{4}/.test(v);
-    //     //   },
-    //     message: (props) => `${props.value} is not a valid phone number!`,
-    // },
-    required: [true, "User phone number required"],
-  },
-  templates: [{ type: Schema.Types.ObjectId, ref: "Template" }],
+    name: {
+        type: String,
+        required: true,
+        max: 100,
+    },
+    city: {
+        type: String,
+        required: true,
+        max: 100,
+    },
+    phoneNumber: {
+        type: String,
+        max: 100,
+        // validate: {
+        //     //   validator: function (v) {
+        //     //     return /\d{3}-\d{3}-\d{4}/.test(v);
+        //     //   },
+        //     message: (props) => `${props.value} is not a valid phone number!`,
+        // },
+        required: [true, "User phone number required"],
+    },
+    templates: [{ type: Schema.Types.ObjectId, ref: "Template" }],
 });
 
 module.exports = mongoose.model("Organization", OrganizationSchema);

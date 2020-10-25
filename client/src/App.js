2a9ee4233a396e32ed2bca385b9e91fe4159f681
HalimoG <halimoghelle@gmail.com>
2020-10-25 01:58:33 -0400

    sign in apge

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Welcome } from './components/Welcome'
+import {Home} from './components/Home'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-   
+    <Router>
+    <div>
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/welcome">Weok</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav> */}
+
+      <Switch>
+        {/* <Route path="/">
+          <Welcome />
+        </Route> */}
+       <Route path="/home">
+          <Home />
+        </Route> 
+        {/* <Route path="/">
+          <Home />
+        </Route>  */}
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Paper} from '@material-ui/core'
+import styled from 'styled-components'
+
+const Container = styled.div ``
+
+export const Home = props => {
+
+return (
+    <Container>
+    <Paper elevation={3} >
+        <p>dsfdsfd</p>
+        </Paper>
+   
+    </Container>
+)
+}

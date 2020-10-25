671e6af8e1bf830da3922b362f64f0ddfa169324
anisa114 <atahlil@loyalty.com>
2020-10-25 00:05:57 -0400

    addressed change requests

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var Users = require('../../db/models/Users')
-var router = express.Router();
-
-const Users = require("../../db/models/Users");
+const express = require('express');
+const Users = require('../../db/models/Users')
+const router = express.Router();
 
 const Address = require("../../db/models/Address");
 
@@ -20,23 +18,36 @@ router.post("/", async (req, res, next) => {
     city: addressBody.city,
     province: addressBody.province,
     postalCode: addressBody.postalCode
+  });
+  address.save().catch((err) => {
+    console.log("Error saving user address ", err)
+    res.sendStatus(500);
   })
   let orgBody = req.body.organization;
-  const organization = new Organization({
-    name: orgBody.name,
-    city: orgBody.city,
-    phoneNumber: orgBody.phoneNumber
+  Organization.findOne({ name: orgBody.name }).then(async (organization) => {
+    const user = new Users({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      address: address,
+      organization: organization
+    });
+    await user.save()
+  }).catch((err) => {
+    console.log("Error saving user ", err)
+    res.sendStatus(500);
   })
-
-  const user = new Users({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    address: address,
-    organization: organization
-  });
-  await user.save()
-
   res.sendStatus(204)
+});
+
+router.delete("/", async (req, res, next) => {
+  let orgId = req.body.organization;
+  Users.remove({ organization: orgId }).then(() => {
+    res.sendStatus(200);
+
+  }).catch((err) => {
+    console.log("Error deleting users ", err);
+    res.sendStatus(500);
+  })
 
 });
 module.exports = router;
